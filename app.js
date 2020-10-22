69b9fffa2489b6c99c58760c43b16668f55fb515
JD Marty <joshua.dmarty@gmail.com>
2020-10-21 19:08:47 -0700

    got slider display and storage object working for advanced search

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
 $(document).ready(function() {
-console.log("rock and roll");
+  //-----------------------------------------------------------------
+  var advancedSearchObj = {};
 
+  function getSource(id) {
+    let queryURL =
+      "https://api.spoonacular.com/recipes/complexSearch?apiKey=233e29c645cf4eaca90809d7a4c85141&query=" +
+      id +
+      "&minCalories=100";
 
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+      // JSON.stringify(response)
+      createNutritionBlock(response);
+    });
+  }
 
+  //create function to write nutrition block and call write nutrition function in ajax
+  function createNutritionBlock(response) {
+    var foodImageDiv = $("<div class='food'>");
+    var actualFoodImage = $("<img>").attr("src", response.results[0].image);
+    console.log(actualFoodImage);
+    foodImageDiv.append(actualFoodImage);
+    console.log(foodImageDiv);
 
+    var actualTitle = $("<h1>").text(response.results[0].title);
+    foodImageDiv.append(actualTitle);
+    $("#foodGoesHere").html(foodImageDiv);
+  }
 
-function getSource(id) {
-let queryURL = "https://api.spoonacular.com/recipes/complexSearch?apiKey=233e29c645cf4eaca90809d7a4c85141&query="+id+ "&minCalories=100";
-
-$.ajax ({
-    url: queryURL,
-    method: "GET" })
-    .then(function(response) {
-        console.log(response);
-        // JSON.stringify(response)
-        createNutritionBlock(response);
-})}
-
-//create function to write nutrition block and call write nutrition function in ajax
-function createNutritionBlock(response){
-  var foodImageDiv = $("<div class='food'>");
-        var actualFoodImage = $("<img>").attr("src", response.results[0].image);
-        console.log(actualFoodImage);
-        foodImageDiv.append(actualFoodImage);
-        console.log(foodImageDiv)
-
-        var actualTitle = $("<h1>").text(response.results[0].title)
-        foodImageDiv.append(actualTitle);
-        $("#foodGoesHere").html(foodImageDiv);
-}
-
-
-$("#searchButton").on("click", function(event) {
+  $("#searchButton").on("click", function (event) {
     // Preventing the button from trying to submit the form
     event.preventDefault();
     // Storing the food name
@@ -40,6 +40,30 @@ $("#searchButton").on("click", function(event) {
     // Running the food function(passing in the food as an argument)
     getSource(inputFood);
   });
+
+  //Listener for hide button
+  $('#advancedSearchBox').hide()
+  $("#advancedMenuToggle").on("click", function (e) {
+    e.preventDefault();
+    $("#advancedSearchBox").slideToggle();
+  });
+
+  //Listener for sliders
+  $("#advancedSearchBox")
+    .find('input[type="range"]')
+    .on("change", function (e) {
+      e.preventDefault();
+      var currentValue = $(e.currentTarget).val();
+      var targetDisplay = $(e.currentTarget).attr("data-slider");
+      $(`[data-display=${targetDisplay}]`).text(currentValue);
+      advancedSearchObj[targetDisplay] = currentValue;
+      console.log(advancedSearchObj);
+    });
+
+
+
+
+//-----------------------------------------------------------------------
 });
 
 // Yelp API call below:
@@ -58,16 +82,3 @@ var yelpQueryURL = "https://api.yelp.com/v3/businesses/"+yelpClientID;
 
 
 
-
-
-
-
-
-// $('#button').on("click", function(event) {
-//     event.preventDefault();
-
-
-// var inputFood = $('#search').val().trim();
-// getSource(inputFood);
-
-// })
