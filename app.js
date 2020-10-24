7c22dbcd8ca0ccc96ee982f61f4c4ef84c8f6686
JD Marty <joshua.dmarty@gmail.com>
2020-10-23 17:15:02 -0700

    changed order of advanced search and added button to save

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ $(document).ready(function() {
   //=======================================================
   //globals
   var advancedSearchObj = {};
+  var searchResults
   var nutritionInformation
 
   //Recipe Search
   function getRecipe(query) {
     var advancedOptions = parseAdvancedSearch()
-    let queryURL = "https://api.spoonacular.com/recipes/complexSearch?apiKey=1679c56d3606492fbf0477a862a3177a&sort=healthiness&sortDirection=desc&addRecipeNutrition=true&query="+query+advancedOptions;
+    let queryURL = "https://api.spoonacular.com/recipes/complexSearch?apiKey=1679c56d3606492fbf0477a862a3177a&sort=random&addRecipeNutrition=true&query="+query+advancedOptions;
     $.ajax ({
         url: queryURL,
         method: "GET" })
       .then(function(response) {
         console.log(response)
+        searchResults = response.results
+        console.log(searchResults)
         nutritionInformation = response.results[0]
+        console.log(nutritionInformation)
         createNutritionBlock(nutritionInformation);
     })
   }
@@ -66,8 +70,7 @@ $(document).ready(function() {
   });
 
   //Listener for sliders
-  $("#advancedSearchBox")
-    .find('input[type="range"]')
+  $('input[type="range"]')
     .on("change", function (e) {
       e.preventDefault();
       var currentValue = $(e.currentTarget).val();
@@ -93,7 +96,7 @@ $(document).ready(function() {
   function parseAdvancedSearch() {
     var optionsString = ''
     if (advancedSearchObj.maxCalories) optionsString += `&maxCalories=${advancedSearchObj.maxCalories}`;
-    if (advancedSearchObj.minProtein) optionsString += `&minProtein=${advancedSearchObj.minProtein}`;
+    if (advancedSearchObj.minProtein) optionsString += `&maxProtein=${advancedSearchObj.minProtein}`;
     if (advancedSearchObj.maxCarbs) optionsString += `&maxCarbs=${advancedSearchObj.maxCarbs}`;
     if (advancedSearchObj.maxSugar) optionsString += `&maxSugar=${advancedSearchObj.maxSugar}`;
     if (advancedSearchObj.diet) optionsString += `&diet=${advancedSearchObj.diet}`;
@@ -108,11 +111,17 @@ $(document).ready(function() {
     return optionsString
   }
 
-  //function
+  //function to reset advanced search options
+  function resetAS() {
+    $('input[type="range"]').val('');
+    $("#dietarySelect").val('None')
+    $('[type="checkbox"]').attr('checked', false);
+  }
+  
 
   //test button
   $('#testAS').on('click', function() {
-
+    resetAS()
   })
   //------------------------------------------------------------------
 
@@ -122,15 +131,15 @@ $(document).ready(function() {
 });
 
 // Yelp API call below:
-var APIkey = "w6fgLGzcpy_01ZiH1W8Wdv69R_KtftS4uTpN-pt6VXrjQQeVISsP88yoeNd8gPFVWbDC2S4g-BRfKBJnQAYsFiTHlPE3HswwZTt7ZCHSSQ6UlupqIkn2TR9RE1OLX3Yx";
-var yelpClientID = "5W6-7gKDvj8VLYIHT96lzQ"
-var yelpQueryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=pizza&location=Irvine";
+// var APIkey = "w6fgLGzcpy_01ZiH1W8Wdv69R_KtftS4uTpN-pt6VXrjQQeVISsP88yoeNd8gPFVWbDC2S4g-BRfKBJnQAYsFiTHlPE3HswwZTt7ZCHSSQ6UlupqIkn2TR9RE1OLX3Yx";
+// var yelpClientID = "5W6-7gKDvj8VLYIHT96lzQ"
+// var yelpQueryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=pizza&location=Irvine";
 
-$.ajax({
-  url: yelpQueryURL,
-  method: "GET",
-  headers: { Authorization: "Bearer " + APIkey}})
-  .then(function(response) {
-      console.log(response);
-  });
+// $.ajax({
+//   url: yelpQueryURL,
+//   method: "GET",
+//   headers: { Authorization: "Bearer " + APIkey}})
+//   .then(function(response) {
+//       console.log(response);
+//   });
 
